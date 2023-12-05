cc12590393cb9fc19cc1002124623a0a43cc9fc4
adamw <adam@warski.org>
2023-12-05 21:17:51 +0100

    WIP

--- bench/bench-java/src/main/java/jox/RendezvousBenchmark.java
+++ bench/bench-java/src/main/java/jox/RendezvousBenchmark.java
@@ -29,7 +29,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.sample;
+package jox;
 
 import jox.Channel;
 import org.openjdk.jmh.annotations.*;
---
+++ bench/bench-kotlin/src/jox/RendezvousKotlinBenchmark.kt
@@ -0,0 +1,35 @@
+package jox
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.openjdk.jmh.annotations.*
+import java.util.concurrent.TimeUnit
+
+// same parameters as in the java benchmark
+@Warmup(iterations = 3, time = 5000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 5000, timeUnit = TimeUnit.MILLISECONDS)
+@Timeout(time = 5100, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(value = 3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+open class RendezvousKotlinBenchmark {
+    @Benchmark
+    @OperationsPerInvocation(OPERATIONS_PER_INVOCATION)
+    fun sendReceiveUsingDefaultDispatcher() {
+        runBlocking {
+            val channel = Channel<Long>(0)
+            launch(Dispatchers.Default) {
+                for (x in 1..OPERATIONS_PER_INVOCATION) channel.send(63)
+                channel.close()
+            }
+
+            launch(Dispatchers.Default) {
+                for (x in 1..OPERATIONS_PER_INVOCATION) channel.receive()
+            }
+        }
+    }
+}
+
+const val OPERATIONS_PER_INVOCATION = 1_000_000
