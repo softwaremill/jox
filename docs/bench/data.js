95ba3df1efd28b9e3c17aa9777b5f38a335e68d3
github-action-benchmark <github@users.noreply.github.com>
2023-12-05 14:06:35 +0000

    add Benchmark (jmh) benchmark result for a5911bc0a816e9ef1aa0c3f247264207cb558f8e

--- docs/bench/data.js
+++ docs/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701785052027,
+  "lastUpdate": 1701785195775,
   "repoUrl": "https://github.com/softwaremill/jox",
   "entries": {
     "Benchmark": [
@@ -82,6 +82,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "org.sample.RendezvousBenchmark.synchronous_queue",
             "value": 8906.81548591961,
+            "unit": "ops/ms",
+            "extra": "iterations: 1\nforks: 1\nthreads: 2"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "adam@warski.org",
+            "name": "adamw",
+            "username": "adamw"
+          },
+          "committer": {
+            "email": "adam@warski.org",
+            "name": "adamw",
+            "username": "adamw"
+          },
+          "distinct": true,
+          "id": "a5911bc0a816e9ef1aa0c3f247264207cb558f8e",
+          "message": "WIP",
+          "timestamp": "2023-12-05T15:06:09+01:00",
+          "tree_id": "40f9d72420a7277a052ed6ee5472e80c77d57ced",
+          "url": "https://github.com/softwaremill/jox/commit/a5911bc0a816e9ef1aa0c3f247264207cb558f8e"
+        },
+        "date": 1701785195759,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "org.sample.RendezvousBenchmark.channel",
+            "value": 10798.01980770764,
+            "unit": "ops/ms",
+            "extra": "iterations: 1\nforks: 1\nthreads: 2"
+          },
+          {
+            "name": "org.sample.RendezvousBenchmark.exchanger",
+            "value": 21325.629436594245,
+            "unit": "ops/ms",
+            "extra": "iterations: 1\nforks: 1\nthreads: 2"
+          },
+          {
+            "name": "org.sample.RendezvousBenchmark.synchronous_queue",
+            "value": 8973.730384836621,
             "unit": "ops/ms",
             "extra": "iterations: 1\nforks: 1\nthreads: 2"
           }
