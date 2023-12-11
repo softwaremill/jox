1cb143a310b7effbc9f60cb603d1ebdf66d8712b
adamw <adam@warski.org>
2023-12-11 09:34:14 +0100

    Count processed cells in segments

--- core/src/test/java/jox/SegmentTest.java
+++ core/src/test/java/jox/SegmentTest.java
@@ -12,10 +12,12 @@ import static jox.TestUtil.scoped;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SegmentTest {
+    // rendezvous segment = where countProcessed is false
+
     @Test
-    void segmentShouldBecomeRemovedOnceAllCellsInterrupted() {
+    void rendezvousSegmentShouldBecomeRemovedOnceAllCellsInterrupted() {
         // given
-        var ss = createSegmentChain(3, 0);
+        var ss = createSegmentChain(3, 0, false);
 
         // when
         for (int i = 0; i < SEGMENT_SIZE - 1; i++) {
@@ -41,9 +43,51 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldRemoveMultipleSegments() {
+    void segmentShouldBecomeRemovedOnceAllCellsInterruptedAndProcessed() {
         // given
-        var ss = createSegmentChain(5, 0);
+        var ss = createSegmentChain(3, 0, true);
+
+        // when
+        // interrupting all cells
+        for (int i = 0; i < SEGMENT_SIZE; i++) {
+            ss[1].cellInterrupted();
+            // nothing should happen
+            assertFalse(ss[1].isRemoved());
+            assertEquals(ss[1].getPrev(), ss[0]);
+            assertEquals(ss[1].getNext(), ss[2]);
+            assertEquals(ss[0].getPrev(), null);
+            assertEquals(ss[0].getNext(), ss[1]);
+            assertEquals(ss[2].getPrev(), ss[1]);
+            assertEquals(ss[2].getNext(), null);
+        }
+
+        // processing all cells but one
+        for (int i = 0; i < SEGMENT_SIZE - 1; i++) {
+            ss[1].cellProcessed();
+            // nothing should happen
+            assertFalse(ss[1].isRemoved());
+            assertEquals(ss[1].getPrev(), ss[0]);
+            assertEquals(ss[1].getNext(), ss[2]);
+            assertEquals(ss[0].getPrev(), null);
+            assertEquals(ss[0].getNext(), ss[1]);
+            assertEquals(ss[2].getPrev(), ss[1]);
+            assertEquals(ss[2].getNext(), null);
+        }
+
+        ss[1].cellProcessed(); // last cell
+        assertTrue(ss[1].isRemoved());
+
+        // then
+        assertEquals(ss[0].getPrev(), null);
+        assertEquals(ss[0].getNext(), ss[2]);
+        assertEquals(ss[2].getPrev(), ss[0]);
+        assertEquals(ss[2].getNext(), null);
+    }
+
+    @Test
+    void shouldRemoveMultipleRendezvousSegments() {
+        // given
+        var ss = createSegmentChain(5, 0, false);
 
         // when
         // first, preventing automatic removal
@@ -77,9 +121,9 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldNotIncrementIncomingPointersIfSegmentRemoved() {
+    void shouldNotIncrementIncomingPointersIfRendezvousSegmentRemoved() {
         // given
-        var ss = createSegmentChain(1, 0);
+        var ss = createSegmentChain(1, 0, false);
 
         // when
         interruptAllCells(ss[0]);
@@ -89,9 +133,9 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldIncrementAndDecrementPointers() {
+    void shouldIncrementAndDecrementPointersInRendezvousSegment() {
         // given
-        var ss = createSegmentChain(1, 0);
+        var ss = createSegmentChain(1, 0, false);
 
         // when
         assertTrue(ss[0].tryIncPointers());
@@ -99,9 +143,9 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldNotRemoveSegmentIfThereAreIncomingPointers() {
+    void shouldNotRemoveRendezvousSegmentIfThereAreIncomingPointers() {
         // given
-        var ss = createSegmentChain(2, 0);
+        var ss = createSegmentChain(2, 0, false);
 
         // when
         assertTrue(ss[0].tryIncPointers());
@@ -121,12 +165,12 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldRemoveSegmentsWhenRunConcurrently() throws ExecutionException, InterruptedException {
+    void shouldRemoveRendezvousSegmentsWhenRunConcurrently() throws ExecutionException, InterruptedException {
         // given
         int segmentCount = 30;
 
         for (int k = 0; k < 1000; k++) {
-            var ss = createSegmentChain(segmentCount, 0);
+            var ss = createSegmentChain(segmentCount, 0, false);
 
             // when
             scoped(scope -> {
@@ -159,9 +203,9 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldNotResurrectAnUnlikedSegment() {
+    void shouldNotResurrectAnUnlikedRendezvousSegment() {
         // given
-        var ss = createSegmentChain(3, 0);
+        var ss = createSegmentChain(3, 0, false);
 
         // when
         // unlinking segment 2 from segment 3
@@ -177,27 +221,27 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldFindAndMoveReferenceForward() {
+    void shouldFindAndMoveRendezvousSegmentReferenceForward() {
         // given
-        var s = createSegmentChain(4, 0);
+        var s = createSegmentChain(4, 0, false);
         var r = new AtomicReference<>(s[0]);
 
         // when
-        var result = Segment.findAndMoveForward(r, s[0], 2);
+        var result = Segment.findAndMoveForward(r, s[0], 2, false);
 
         // then
         assertEquals(s[2], result);
 
         // when
-        result = Segment.findAndMoveForward(r, s[0], 5);
+        result = Segment.findAndMoveForward(r, s[0], 5, false);
         assertEquals(5, result.getId());
         assertEquals(result, s[3].getNext().getNext());
     }
 
     @Test
-    void shouldRemoveOldTailSegment() {
+    void shouldRemoveOldTailRendezvousSegment() {
         // given
-        var ss = createSegmentChain(2, 0);
+        var ss = createSegmentChain(2, 0, false);
 
         // when
         interruptAllCells(ss[1]);
@@ -207,7 +251,7 @@ public class SegmentTest {
         assertEquals(ss[0].getNext(), ss[1]); // logically, but not physically removed
 
         // when
-        var s2 = Segment.findAndMoveForward(new AtomicReference<>(ss[0]), ss[0], 2);
+        var s2 = Segment.findAndMoveForward(new AtomicReference<>(ss[0]), ss[0], 2, false);
 
         // then
         assertEquals(s2, ss[0].getNext());
@@ -215,26 +259,26 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldReturnNextSegmentIfRemoved() {
+    void shouldReturnNextRendezvousSegmentIfRemoved() {
         // given
-        var ss = createSegmentChain(3, 0);
+        var ss = createSegmentChain(3, 0, false);
         interruptAllCells(ss[1]);
 
         // when
-        var result = Segment.findAndMoveForward(new AtomicReference<>(ss[0]), ss[0], 1);
+        var result = Segment.findAndMoveForward(new AtomicReference<>(ss[0]), ss[0], 1, false);
 
         // then
         assertEquals(ss[2], result);
     }
 
     @Test
-    void shouldNotUpdateIfAlreadyUpdated() {
+    void shouldNotUpdateRendezvousSegmentReferenceIfAlreadyUpdated() {
         // given
-        var ss = createSegmentChain(3, 0);
+        var ss = createSegmentChain(3, 0, false);
         var ref = new AtomicReference<>(ss[2]);
 
         // when
-        var result = Segment.findAndMoveForward(ref, ss[0], 1);
+        var result = Segment.findAndMoveForward(ref, ss[0], 1, false);
 
         // then
         assertEquals(ss[1], result);
@@ -242,20 +286,20 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldUpdatePointersWhenReferenceChanges() {
+    void shouldUpdateRendezvousSegmentPointersWhenReferenceChanges() {
         // given
-        var ss = createSegmentChain(3, 0);
+        var ss = createSegmentChain(3, 0, false);
         var ref = new AtomicReference<>(ss[0]);
 
         // when
-        Segment.findAndMoveForward(ref, ss[0], 1);
+        Segment.findAndMoveForward(ref, ss[0], 1, false);
 
         // then
         interruptAllCells(ss[1]);
         assertFalse(ss[1].isRemoved()); // shouldn't be removed because there's an incoming pointer
 
         // when
-        Segment.findAndMoveForward(ref, ss[0], 2);
+        Segment.findAndMoveForward(ref, ss[0], 2, false);
 
         // then
         assertTrue(ss[1].isRemoved()); // no more pointers -> logically removed
@@ -264,10 +308,10 @@ public class SegmentTest {
     }
 
     @Test
-    void shouldConcurrentlyMoveSegmentsForward() throws ExecutionException, InterruptedException {
+    void shouldConcurrentlyMoveRendezvousSegmentsForward() throws ExecutionException, InterruptedException {
         // given
         for (int k = 0; k < 1000; k++) {
-            var ss = createSegmentChain(1, 0);
+            var ss = createSegmentChain(1, 0, false);
             var ref = new AtomicReference<>(ss[0]);
             var observedSegments = new ConcurrentHashMap<Integer, Segment>();
 
@@ -278,7 +322,7 @@ public class SegmentTest {
                     forkVoid(scope, () -> {
                         var s = ss[0];
                         for (int i = 0; i < 300; i++) {
-                            s = Segment.findAndMoveForward(ref, s, i);
+                            s = Segment.findAndMoveForward(ref, s, i, false);
                             var previous = observedSegments.put(i, s);
                             if (previous != s && previous != null) {
                                 fail("Already observed segment: " + previous + " for id: " + i + ", but found: " + s);
@@ -290,12 +334,12 @@ public class SegmentTest {
         }
     }
 
-    private Segment[] createSegmentChain(int count, long id) {
+    private Segment[] createSegmentChain(int count, long id, boolean countProcessed) {
         var segments = new Segment[count];
-        var thisSegment = new Segment(id, null, 0);
+        var thisSegment = new Segment(id, null, 0, countProcessed);
         segments[0] = thisSegment;
         for (int i = 1; i < count; i++) {
-            var nextSegment = new Segment(id + i, thisSegment, 0);
+            var nextSegment = new Segment(id + i, thisSegment, 0, countProcessed);
             thisSegment.setNext(nextSegment);
             segments[i] = nextSegment;
             thisSegment = nextSegment;
