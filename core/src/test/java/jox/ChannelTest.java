9b38c4f7343bccc030c78acf7ffba142d84ad97c
adamw <adam@warski.org>
2023-12-13 11:53:13 +0100

    Stress test

--- core/src/test/java/jox/ChannelTest.java
+++ core/src/test/java/jox/ChannelTest.java
@@ -1,18 +1,12 @@
 package jox;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.util.HashSet;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
-import static jox.TestUtil.*;
+import static jox.TestUtil.forkVoid;
+import static jox.TestUtil.scoped;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -69,8 +63,4 @@ public class ChannelTest {
     }
 }
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@ParameterizedTest
-@ValueSource(strings = {"0", "1", "2", "10"})
-@interface TestWithCapacities {}
+
