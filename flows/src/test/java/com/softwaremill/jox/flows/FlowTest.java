add22901d3b510b73797e655d9ed49b28563f300
emilb <bartnik.emil@gmail.com>
2024-12-19 15:57:55 +0100

    Implement more flows ops methods part 1 (#74)

--- flows/src/test/java/com/softwaremill/jox/flows/FlowTest.java
+++ flows/src/test/java/com/softwaremill/jox/flows/FlowTest.java
@@ -1,14 +1,26 @@
 package com.softwaremill.jox.flows;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.softwaremill.jox.Channel;
 import com.softwaremill.jox.ChannelClosedException;
+import com.softwaremill.jox.ChannelError;
+import com.softwaremill.jox.Source;
+import com.softwaremill.jox.structured.Scopes;
 import org.junit.jupiter.api.Test;
 
 class FlowTest {
@@ -226,5 +238,423 @@ class FlowTest {
 
         // then
         assertThrows(RuntimeException.class, () -> mapped.runForeach(i -> {}));
+    }
+
+    @Test
+    void shouldNotIntersperseWithInjectOnlyOverEmptySource() throws Exception {
+        // given
+        Flow<String> f = Flows.empty();
+
+        // when & then
+        assertEquals(List.of(), f.intersperse(", ").runToList());
+    }
+
+    @Test
+    void shouldNotIntersperseWithInjectOnlyOverSourceWithOneElement() throws Exception {
+        // given
+        Flow<String> f = Flows.fromValues("foo");
+
+        // when & then
+        assertEquals(List.of("foo"), f.intersperse(", ").runToList());
+    }
+
+    @Test
+    void shouldIntersperseWithInjectOnlyOverSourceWithMultipleElements() throws Exception {
+        // given
+        Flow<String> f = Flows.fromValues("foo", "bar");
+
+        // when & then
+        assertEquals(List.of("foo", ", ", "bar"), f.intersperse(", ").runToList());
+    }
+
+    @Test
+    void shouldIntersperseWithStartInjectAndEndOverEmptySource() throws Exception {
+        // given
+        Flow<String> f = Flows.empty();
+
+        // when & then
+        assertEquals(List.of("[", "]"), f.intersperse("[", ", ", "]").runToList());
+    }
+
+    @Test
+    void shouldIntersperseWithStartInjectAndEndOverSourceWithOneElement() throws Exception {
+        // given
+        Flow<String> f = Flows.fromValues("foo");
+
+        // when & then
+        assertEquals(List.of("[", "foo", "]"), f.intersperse("[", ", ", "]").runToList());
+    }
+
+    @Test
+    void shouldIntersperseWithStartInjectAndEndOverSourceWithMultipleElements() throws Exception {
+        // given
+        Flow<String> f = Flows.fromValues("foo", "bar");
+
+        // when & then
+        assertEquals(List.of("[", "foo", ", ", "bar", "]"), f.intersperse("[", ", ", "]").runToList());
+    }
+
+    @Test
+    void shouldNotTakeFromEmptyFlow() throws Exception {
+        // given
+        Flow<Integer> flow = Flows.empty();
+        flow = flow
+                .takeWhile(x -> x < 3, false);
+
+        // when & then
+        assertEquals(List.of(), flow.runToList());
+    }
+
+    @Test
+    void shouldTakeAsLongAsPredicateIsSatisfied() throws Exception {
+        // given
+        Flow<Integer> flow = Flows.fromValues(1, 2, 3)
+                .takeWhile(x -> x < 3, false);
+
+        // when & then
+        assertEquals(List.of(1, 2), flow.runToList());
+    }
+
+    @Test
+    void shouldTakeFailedElementIfIncludeFirstFailingTrue() throws Exception {
+        // given
+        Flow<Integer> flow = Flows.fromValues(1, 2, 3, 4).takeWhile(x -> x < 3, true);
+
+        // when & then
+        assertEquals(List.of(1, 2, 3), flow.runToList());
+    }
+
+    @Test
+    void shouldWorkIfAllElementsMatchPredicate() throws Exception {
+        // given
+        Flow<Integer> flow = Flows.fromValues(1, 2, 3).takeWhile(x -> x < 5, false);
+
+        // when & then
+        assertEquals(List.of(1, 2, 3), flow.runToList());
+    }
+
+    @Test
+    void shouldNotTakeIfPredicateFailsForFirstOrMoreElements() throws Exception {
+        // given
+        Flow<Integer> flow = Flows.fromValues(3, 2, 1).takeWhile(x -> x < 3, false);
+
+        // when & then
+        assertEquals(List.of(), flow.runToList());
+    }
+
+    @Test
+    void shouldNotThrottleEmptySource() throws Exception {
+        // given
+        Flow<Integer> s = Flows.empty();
+
+        // when
+        long startTime = System.currentTimeMillis();
+        List<Integer> result = s.throttle(1, Duration.ofSeconds(1)).runToList();
+        long executionTime = System.currentTimeMillis() - startTime;
+
+        // then
+        assertEquals(Collections.emptyList(), result);
+        assertTrue(executionTime < 200);
+    }
+
+    @Test
+    void shouldThrottleToSpecifiedElementsPerTimeUnits() throws Exception {
+        // given
+        Flow<Integer> s = Flows.fromValues(1, 2);
+
+        // when
+        long startTime = System.currentTimeMillis();
+        List<Integer> result = s.throttle(1, Duration.ofMillis(50)).runToList();
+        long executionTime = System.currentTimeMillis() - startTime;
+
+        // then
+        assertEquals(Arrays.asList(1, 2), result);
+        assertTrue(executionTime >= 100 && executionTime <= 150);
+    }
+
+    @Test
+    void shouldFailToThrottleWhenElementsLessThanOrEqualToZero() {
+        // given
+        Flow<Integer> s = Flows.empty();
+
+        // when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> s.throttle(-1, Duration.ofMillis(50)));
+
+        // then
+        assertEquals("requirement failed: elements must be > 0", exception.getMessage());
+    }
+
+    @Test
+    void shouldFailToThrottleWhenPerLowerThan1ms() {
+        // given
+        Flow<Integer> s = Flows.empty();
+
+        // when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> s.throttle(1, Duration.ofNanos(50)));
+
+        // then
+        assertEquals("requirement failed: per time must be >= 1 ms", exception.getMessage());
+    }
+
+    @Test
+    void shouldNotEvaluateSubsequentFlowsIfThereIsAFailure() {
+        // given
+        AtomicBoolean evaluated = new AtomicBoolean(false);
+        Flow<Integer> f = Flows.<Integer>failed(new IllegalStateException())
+                .concat(Flows.usingEmit(emit -> {
+                    evaluated.set(true);
+                    emit.apply(1);
+                }));
+
+        // when & then
+        assertThrows(IllegalStateException.class, f::runToList);
+        assertFalse(evaluated.get());
+    }
+
+    @Test
+    void shouldConcatFlows() throws Exception {
+        // given
+        Flow<Integer> f1 = Flows.fromValues(1, 2, 3);
+        Flow<Integer> f2 = Flows.fromValues(4, 5, 6);
+
+        // when
+        Flow<Integer> f = f1.concat(f2);
+
+        // then
+        assertEquals(List.of(1, 2, 3, 4, 5, 6), f.runToList());
+    }
+
+    @Test
+    void shouldPrependFlows() throws Exception {
+        // given
+        Flow<Integer> f1 = Flows.fromValues(1, 2, 3);
+        Flow<Integer> f2 = Flows.fromValues(4, 5, 6);
+
+        // when
+        Flow<Integer> f = f1.prepend(f2);
+
+        // then
+        assertEquals(List.of(4, 5, 6, 1, 2, 3), f.runToList());
+    }
+
+    @Test
+    void shouldNotDropFromEmptyFlow() throws Exception {
+        // given
+        Flow<Integer> s = Flows.empty();
+
+        // when & then
+        assertEquals(List.of(), s.drop(1).runToList());
+    }
+
+    @Test
+    void shouldDropElementsFromSource() throws Exception {
+        // given
+        Flow<Integer> s = Flows.fromValues(1, 2, 3);
+
+        // when & then
+        assertEquals(List.of(3), s.drop(2).runToList());
+    }
+
+    @Test
+    void shouldReturnEmptySourceWhenMoreElementsThanSourceLengthWasDropped() throws Exception {
+        // given
+        Flow<Integer> s = Flows.fromValues(1, 2);
+
+        // when & then
+        assertEquals(List.of(), s.drop(3).runToList());
+    }
+
+    @Test
+    void shouldNotDropWhenNIsZero() throws Exception {
+        // given
+        Flow<Integer> s = Flows.fromValues(1, 2, 3);
+
+        // when & then
+        assertEquals(List.of(1, 2, 3), s.drop(0).runToList());
+    }
+
+    @Test
+    void shouldMergeTwoSimpleFlows() throws Exception {
+        // given
+        var c1 = Flows.fromValues(1, 2, 3);
+        var c2 = Flows.fromValues(4, 5, 6);
+
+        // when
+        List<Integer> result = c1.merge(c2, false, false).runToList()
+                .stream()
+                .sorted()
+                .toList();
+
+        // then
+        assertEquals(List.of(1, 2, 3, 4, 5, 6), result);
+    }
+
+    @Test
+    void shouldMergeTwoAsyncFlows() throws Exception {
+        // given
+        var c1 = Flows.fromValues(1, 2, 3).buffer(10);
+        var c2 = Flows.fromValues(4, 5, 6).buffer(10);
+
+        // when
+        List<Integer> result = c1.merge(c2, false, false).runToList()
+                .stream()
+                .sorted()
+                .toList();
+
+        // then
+        assertEquals(List.of(1, 2, 3, 4, 5, 6), result);
+    }
+
+    @Test
+    void shouldMergeWithATickFlow() throws Exception {
+        // given
+        var c1 = Flows.tick(Duration.ofMillis(100), 0).take(3);
+        var c2 = Flows.fromValues(0, 1, 2, 3);
+
+        // when
+        var r = c1.merge(c2, false, false).runToList();
+
+        // then
+        assertThat(new HashSet<>(r), contains(0, 1, 2, 3));
+        assertEquals(List.of(0, 0), r.subList(r.size() - 2, r.size()));
+    }
+
+    @Test
+    void shouldPropagateErrorFromTheLeft() {
+        // given
+        var c1 = Flows.fromValues(1, 2, 3).concat(Flows.<Integer>failed(new IllegalStateException()));
+        var c2 = Flows.fromValues(4, 5, 6);
+
+        var s = c1.merge(c2, false, false);
+
+        // when
+        var exception = assertThrows(ChannelClosedException.class, s::runToList);
+
+        // then
+        assertInstanceOf(IllegalStateException.class, exception.getCause());
+    }
+
+    @Test
+    void shouldPropagateErrorFromTheRight() {
+        // given
+        var c1 = Flows.fromValues(1, 2, 3);
+        var c2 = Flows.fromValues(4, 5, 6).concat(Flows.<Integer>failed(new IllegalStateException()));
+
+        var s = c1.merge(c2, false, false);
+
+        // when
+        var exception = assertThrows(ChannelClosedException.class, s::runToList);
+
+        // then
+        assertInstanceOf(IllegalStateException.class, exception.getCause());
+    }
+
+    @Test
+    void shouldMergeTwoFlowsEmittingAllElementsFromTheLeftWhenRightCompletes() throws Exception {
+        // given
+        var c1 = Flows.fromValues(1, 2, 3, 4).throttle(1, Duration.ofMillis(100));
+        var c2 = Flows.fromValues(5, 6).throttle(1, Duration.ofMillis(100));
+
+        // when
+        List<Integer> result = c1.merge(c2, false, false).runToList()
+                .stream()
+                .sorted()
+                .toList();
+
+        // then
+        assertEquals(List.of(1, 2, 3, 4, 5, 6), result);
+    }
+
+    @Test
+    void shouldMergeTwoFlowsEmittingAllElementsFromTheRightWhenLeftCompletes() throws Exception {
+        // given
+        var c1 = Flows.fromValues(1, 2).throttle(1, Duration.ofMillis(100));
+        var c2 = Flows.fromValues(3, 4, 5, 6).throttle(1, Duration.ofMillis(100));
+
+        // when
+        List<Integer> result = c1.merge(c2, false, false).runToList()
+                .stream()
+                .sorted()
+                .toList();
+
+        // then
+        assertEquals(List.of(1, 2, 3, 4, 5, 6), result);
+    }
+
+    @Test
+    void shouldMergeTwoFlowsCompletingTheResultingFlowWhenTheLeftFlowCompletes() throws Exception {
+        // given
+        var c1 = Flows.fromValues(1, 2).throttle(1, Duration.ofMillis(100));
+        var c2 = Flows.fromValues(3, 4, 5, 6).throttle(1, Duration.ofMillis(100));
+
+        // when
+        var result = c1.merge(c2, true, false).runToList()
+                .stream()
+                .sorted()
+                .toList();
+
+        // then
+        assertTrue(result.equals(List.of(1, 2, 3, 4)) || result.equals(List.of(1, 2, 3)));
+    }
+
+    @Test
+    void shouldMergeTwoFlowsCompletingTheResultingFlowWhenTheRightFlowCompletes() throws Exception {
+        // given
+        var c1 = Flows.fromValues(1, 2, 3, 4).throttle(1, Duration.ofMillis(100));
+        var c2 = Flows.fromValues(5, 6).throttle(1, Duration.ofMillis(100));
+
+        // when
+        var result = c1.merge(c2, false, true)
+                .runToList()
+                .stream()
+                .sorted()
+                .toList();
+
+        // then
+        assertTrue(result.equals(List.of(1, 2, 5, 6)) || result.equals(List.of(1, 2, 3, 5, 6)));
+    }
+
+    @Test
+    void shouldEmitElementsOnlyFromOriginalSourceWhenNotEmpty() throws Exception {
+        // given
+        Flow<Integer> flow = Flows.fromValues(1)
+                .orElse(Flows.fromValues(2, 3));
+
+        // when & then
+        assertEquals(List.of(1), flow.runToList());
+    }
+
+    @Test
+    void shouldEmitElementsOnlyFromAlternativeSourceWhenOriginalSourceCreatedEmpty() throws Exception {
+        // given
+        Flow<Integer> flow = Flows.<Integer>empty().orElse(Flows.fromValues(2, 3));
+
+        // when & then
+        assertEquals(List.of(2, 3), flow.runToList());
+    }
+
+    @Test
+    void shouldEmitElementsOnlyFromAlternativeSourceWhenOriginalSourceEmpty() throws Exception {
+        // given
+        Flow<Integer> flow = Flows.<Integer>fromValues().orElse(Flows.fromValues(2, 3));
+
+        // when & then
+        assertEquals(List.of(2, 3), flow.runToList());
+    }
+
+    @Test
+    void shouldReturnFailedSourceWhenOriginalSourceFailed() throws Exception {
+        // given
+        RuntimeException failure = new RuntimeException();
+        Flow<Integer> flow = Flows.<Integer>failed(failure).orElse(Flows.fromValues(2, 3));
+
+        // when & then
+        Scopes.unsupervised(scope -> {
+            Source<Integer> source = flow.runToChannel(scope);
+            assertEquals(failure, ((ChannelError) source.receiveOrClosed()).cause());
+            return null;
+        });
     }
 }
