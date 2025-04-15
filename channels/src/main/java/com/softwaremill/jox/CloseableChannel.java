f3c523483bb08d1b33d81faca681ef3f88de6f7a
adamw <adam@warski.org>
2025-04-15 10:44:49 +0200

    Fix docs

--- channels/src/main/java/com/softwaremill/jox/CloseableChannel.java
+++ channels/src/main/java/com/softwaremill/jox/CloseableChannel.java
@@ -91,8 +91,7 @@ public interface CloseableChannel {
      * <p>
      * When {@code false}, values <strong>might</strong> be received from the channel, when calling
      * {@link Source#receive()}, but it's not guaranteed that some values will be available. They might be received
-     * concurrently, or filtered out if the channel is created using {@link Source#collectAsView(Function)} or
-     * {@link Source#filterAsView(Predicate)}.
+     * concurrently.
      */
     default boolean isClosedForReceive() {
         return closedForReceive() != null;
